ffb5b4abc1706963a04a7f1adbfc7b9e5b4f228f
billythedummy <dhy1996@live.com.sg>
2025-06-12 18:15:21 +0800

    add traits for err

--- core/src/error.rs
+++ core/src/error.rs
@@ -1,3 +1,6 @@
+use core::{error::Error, fmt::Display};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum SplStakePoolError {
     CalculationFailure,
     IncorrectDepositVoteAddress,
@@ -6,3 +9,12 @@ pub enum SplStakePoolError {
     SolWithdrawalTooLarge,
     StakeListAndPoolOutOfDate,
 }
+
+impl Display for SplStakePoolError {
+    // Display=Debug, since this is just a simple str enum
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "{self:#?}")
+    }
+}
+
+impl Error for SplStakePoolError {}
